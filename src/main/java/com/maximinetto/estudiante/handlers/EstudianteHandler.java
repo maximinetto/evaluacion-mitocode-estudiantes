34d8ed4a9c7905033d06c8ea9c1f1553223ff32f
Maximiliano Minetto <maximinetto@gmail.com>
2020-03-18 10:25:01 -0300

    Listar por id

--- src/main/java/com/maximinetto/estudiante/handlers/EstudianteHandler.java
+++ src/main/java/com/maximinetto/estudiante/handlers/EstudianteHandler.java
@@ -1,8 +1,10 @@
 package com.maximinetto.estudiante.handlers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -14,6 +16,9 @@ import reactor.core.publisher.Mono;
 @Component
 public class EstudianteHandler {
     
+    @Value("${id.name}")
+    private String id;
+    
     @Autowired
     private EstudianteService service;
     
@@ -21,6 +26,17 @@ public class EstudianteHandler {
 	return ServerResponse.ok()
 		.contentType(MediaType.APPLICATION_STREAM_JSON)
 		.body(service.getAll(), Estudiante.class);
+    }
+    
+    public Mono<ServerResponse> listarPorId(ServerRequest request){
+	String idEstudiante = request.pathVariable(id);
+	return service.get(idEstudiante)
+		.flatMap( e -> ServerResponse.ok()
+			   .contentType(MediaType.APPLICATION_STREAM_JSON)
+			   .body(BodyInserters.fromValue(e))
+		)
+		.switchIfEmpty(ServerResponse.notFound().build());
+	
     }
     
 }
--- src/main/java/com/maximinetto/estudiante/routers/RouterEstudiante.java
+++ src/main/java/com/maximinetto/estudiante/routers/RouterEstudiante.java
@@ -5,15 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+import org.springframework.beans.factory.annotation.Value;
+
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 
 import com.maximinetto.estudiante.handlers.EstudianteHandler;
 
 @Configuration
 public class RouterEstudiante {
+    @Value("${id.name}")
+    private String id;
     
     @Bean
     public RouterFunction<ServerResponse> rutas(EstudianteHandler handler){
-	return route(GET("/estudiantes"), handler::listar);
+	return route(GET("/estudiantes"), handler::listar)
+		.andRoute(GET(String.format("/estudiantes/{%s}", id)), handler::listarPorId);
     }
+    
+    
 }
